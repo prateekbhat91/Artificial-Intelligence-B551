5fd8c6a4068265299afd310a7da093fffa4cb1eb
prateekbhat91 <prateekbhat91@gmail.com>
2016-01-21 13:24:29 -0500

    BFS

--- Assignment 1.py
+++ Assignment 1.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 import sys
+import Queue
 
 vertices = {}
 
@@ -8,7 +9,7 @@ class node():
 
     def __init__(self, id):
         self.id = id
-        self.adjacent = {} 
+        self.adjacent = {}
 
     "Function to set the adjacent nodes for vertex in the form of a dictionary"
     def set_adjacent(self,set):
@@ -43,15 +44,51 @@ def readfile(link):
     for lines in file.readlines():
         arr = lines.split()
         # raw_input()
-        dict[arr[0]].append((arr[1], int(arr[2])))
+        dict[arr[0].lower()].append((arr[1].lower(), int(arr[2])))
     file.close()
     return dict
+
+"Breadth First search to find the paths from Start to Goal state"
+def bfs(vertices, start, goal):
+    startN = vertices[start]
+    goalN = vertices[goal]
+    q = Queue.Queue()
+    # path = [startN]
+    q.put((startN,[startN],0))
+
+    while not q.empty():
+        (vertex,path,dist) = q.get()
+        # raw_input()
+        # for items in vertex.adjacent.keys():
+        #     print items.id
+        # for items in path:
+        #     print items.id
+        for items in list(set(vertex.adjacent.keys()) - set(path)):
+            # print items.id
+            path1 = path
+            if items == goalN:
+                print "The path is: ",
+                for n in path1+[items]:
+                    print n.id,
+                print "\nThe distance is: ", dist+vertex.adjacent[items]
+                # print path1+[items],dist+vertex.adjacent[items]
+            else:
+                # raw_input()
+                # path1.append(items)
+                # print "item",items.id
+                # print "path",
+                # for items in path1:
+                #     print items.id,
+                # print "dist", dist+vertex.adjacent[items]
+                q.put((items,path1+[items],dist+vertex.adjacent[items]))
 
 
 if __name__ == "__main__":
     ar = readfile("distance_matrix.txt")
     print ar
     make_node(ar)
+    # print "Enter Start and the Goal cities"
+    bfs(vertices,"arad", "bucharest")
     # for k in vertices.keys():
     #     # print k
     #     print vertices[k].id
